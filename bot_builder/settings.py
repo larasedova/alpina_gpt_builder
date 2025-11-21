5522f7701451d245ed324fce056bf83b52294f57
larasedova <lara.sedova.00@mail.ru>
2025-11-21 15:12:16 +0300

    Add db/ directory for SQLite

--- bot_builder/settings.py
+++ bot_builder/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3', # Путь внутри контейнера web
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
