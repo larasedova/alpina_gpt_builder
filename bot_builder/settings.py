4efec687df818b2a35f54c8c7cedcddd4602aaf7
larasedova <lara.sedova.00@mail.ru>
2025-11-21 14:57:41 +0300

    Fix settings.py

--- bot_builder/settings.py
+++ bot_builder/settings.py
@@ -153,17 +153,12 @@ if not DEBUG:
         'version': 1,
         'disable_existing_loggers': False,
         'handlers': {
-            'file': {
-                'level': 'ERROR',
-                'class': 'logging.FileHandler',
-                'filename': BASE_DIR / 'django_errors.log',
+            'console': {
+                'class': 'logging.StreamHandler',
             },
         },
-        'loggers': {
-            'django': {
-                'handlers': ['file'],
-                'level': 'ERROR',
-                'propagate': True,
-            },
+        'root': {
+            'handlers': ['console'],
+            'level': 'INFO',
         },
     }
